84bd1fa3114800b5aa87712126a745006968be51
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2020-01-27 14:04:55 -0800

    add documentation

--- R/add_col.R
+++ R/add_col.R
@@ -1,3 +1,16 @@
+#' Add column into an existing datafrma
+#'
+#' Cool, huh?
+#'
+#' @param df1 A dataframe
+#' @param name A character arg
+#' @param value something
+#' @param where Integer value
+#'
+#' @return
+#' @export
+#'
+#' @examples
 add_col <- function(df1, name, value, where){
 
     df2 <- data.frame(y = value)
--- R/insert_into.R
+++ R/insert_into.R
@@ -1,3 +1,15 @@
+#' Insert one dataframe into another
+#'
+#' You can specify where to splice in
+#'
+#' @param x A dataframe
+#' @param y A dataframe
+#' @param where An integer value
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
 insert_into <- function(x, y, where = 1) {
     if (where == 1) {
         cbind(y, x)
